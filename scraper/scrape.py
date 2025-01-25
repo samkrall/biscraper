917e93b560856173a186765d26f563061ecab625
samkrall <95418265+samkrall@users.noreply.github.com>
2025-01-25 06:12:13 -0600

    Update scrape.py

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -4,9 +4,14 @@ import os
 from zipfile import ZipFile
 import shutil
 import ast
+import openpyxl
 
-filepath = 'C:/Users/samdk/Downloads/'
-filename = 'sss.pbit'
+filepath = r"\\ad.sjm.com\cvd\CVD Departments\Market Research Core Team\Sam\NMD Project\\".replace('\\', '/')
+directory_path = os.path.dirname(filepath)
+os.chdir(directory_path)
+print("New Directory:", os.getcwd())
+
+filename = 'NMD OEM Model.pbit'
 dms_path = 'DataModelSchema'
 layout_path = 'Report/Layout'
 f = ZipFile(filepath+filename)
@@ -30,10 +35,22 @@ source_list_df = pd.DataFrame(source_list, columns = ['Table', 'Query'])
 # relationships into a dataframe
 Relationship_list = []
 for relationship in dms['model']['relationships']:
+    print(relationship)
     Relationship = []
-    Relationship.append(relationship['fromTable']+'.'+relationship['fromColumn'] + ' to ' +relationship['toTable']+'.'+relationship['toColumn'])
+    Relationship.append(relationship['fromTable']+'.'+relationship['fromColumn'])
+    Relationship.append(relationship['toTable']+'.'+relationship['toColumn'])
+    try:
+        Relationship.append(relationship['fromCardinality'])
+    except:
+        Relationship.append('Many-to-One')
+    try:
+        Relationship.append(relationship['crossFilteringBehavior'])
+    except:
+        Relationship.append('Single')
+    
     Relationship_list.append(Relationship)
-relationship_list_df = pd.DataFrame(Relationship_list, columns = ['Relationship'])
+relationship_list_df = pd.DataFrame(Relationship_list, columns = ['From', 'To', 'Cardinality', 'CrossFiltering'])
+relationship_list_df['Cardinality'] = relationship_list_df['Cardinality'].apply(lambda x: 'One-to-One' if x == 'one' else x)
 
 # columns into a dataframe
 Column_list = []
@@ -49,15 +66,21 @@ column_list_df = pd.DataFrame(Column_list, columns = ['Table','Column'])
 # measures into a dataframe
 Measure_list = []
 for table in dms['model']['tables']:
-    try:
+    try: 
         for measure in table['measures']:
             Measure = []
             Measure.append(table['name'])
-            Measure.append(measure['name']+': ' + measure['expression'])
+            Measure.append(measure['name'])
+            Measure.append(','.join(measure['expression'][1:]).replace(',', '').strip())
+            joined_string = ','.join(measure['expression'])
             Measure_list.append(Measure)
+    
     except:
-        continue
-measure_list_df = pd.DataFrame(Measure_list, columns = ['Table', 'Measure'])
+            continue
+
+ 
+        
+measure_list_df = pd.DataFrame(Measure_list, columns = ['Table', 'MeasureName', 'Expression'])
 
 visual_list = []
 visual_ignore_list = ['actionButton', 'basicShape', 'image', 'textbox']
@@ -79,4 +102,11 @@ visual_df = pd.DataFrame(visual_list, columns = ['Page', 'Visual', 'Table.Column
 print(visual_df.shape)
 
 #remove temp folder when finished
-#shutil.rmtree(filepath+'/temp')
+shutil.rmtree(filepath+'/temp')
+with pd.ExcelWriter('output.xlsx', engine='openpyxl') as writer:
+    # Write each dataframe to a different worksheet
+    source_list_df.to_excel(writer, sheet_name='Sources', index=False)
+    relationship_list_df.to_excel(writer, sheet_name='Relationships', index=False)
+    column_list_df.to_excel(writer, sheet_name='Columns', index=False)
+    measure_list_df.to_excel(writer, sheet_name='Measures', index=False)
+    visual_df.to_excel(writer, sheet_name='Visuals', index=False)
